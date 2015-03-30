8177b8d73de31393bb62a780ed6da812f24e5e18
Rob McCardle <rmc@Robs-MacBook-Pro.local>
2015-03-30 12:35:20 +0100

    Changed applications

--- modules/people/manifests/robmccardle/applications.pp
+++ modules/people/manifests/robmccardle/applications.pp
@@ -1,8 +1,23 @@
 class people::robmccardle::applications {
   include chrome
   include onepassword
-  include vagrant
-  include virtualbox
+  include onepassword::chrome
+  include firefox
+  include sourcetree
   include iterm2::stable
   include dropbox
+  include spectacle
+  include vlc
+  include phantomjs
+  include nmap
+
+  # For the latest version of v2
+  include sublime_text::v2
+  sublime_text::v2::package { 'Emmet':
+    source => 'sergeche/emmet-sublime'
+  }
+
+  include adobe_creative_cloud
+  include skype
+  include php
 }
