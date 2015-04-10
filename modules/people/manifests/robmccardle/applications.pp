2d66785998745cce3c6cb6814c3e47aaecd19e81
Rob McCardle <rob@enigma-marketing.co.uk>
2015-04-10 16:39:10 +0100

    edits

--- modules/people/manifests/robmccardle/applications.pp
+++ modules/people/manifests/robmccardle/applications.pp
@@ -21,20 +21,67 @@ class people::robmccardle::applications {
   package { 'filezilla': provider => 'brewcask' }
   package { 'charles': provider => 'brewcask' }
 
-  # OSX tweaks
+
+
+  # OSX Global tweaks 
+  # TODO split into OSX file/class
+
+  # Expand the print dialog by default
+  include osx::global::expand_print_dialog 
+  
+  # Expand the save dialog by default
+  include osx::global::expand_save_dialog
+
+  # Disable remote control infrared receiver
+  include osx::global::disable_remote_control_ir_receiver
+
+  # Enable OS zoom
+  include osx::universal_access::ctrl_mod_zoom
+
+
+  # OSX Dock tweaks
+
+  # Use the old flat dock style
+  include osx::dock::2d 
+
+  # Automatically hide the dock
+  include osx::dock::autohide
+  
+  # Ensures the dock only contains apps that are running
+  include osx::dock::clear_dock
+  
+  # Disable the dock by setting a long autohide-delay
+  include osx::dock::disable
+  
+  # Disable the dashboard
+  include osx::dock::disable_dashboard
+  
+  # Dims icons of hidden apps
+  include osx::dock::dim_hidden_apps
+  
+  # Remove the indicator lights below running apps
+  include osx::dock::hide_indicator_lights
+  
+
+  # OSX Finder tweaks
+
   include osx::finder::show_all_filename_extensions
   include osx::finder::unhide_library
   include osx::finder::show_hidden_files
+  include osx::finder::empty_trash_securely
+  # Show all mounted drives/networks etc... on Desktop
+  include osx::finder::show_all_on_desktop
+
+  include osx::finder::show_warning_before_emptying_trash
+  include osx::finder::show_warning_before_changing_an_extension
+  include osx::finder::show_all_filename_extensions
+  include osx::finder::no_file_extension_warnings
+
 
   # Make the top right corner start the screen saver and the bottom left corner launch Mission Control
   class { 'osx::dock::hot_corners':
     top_right => "Start Screen Saver",
     bottom_left => "Mission Control"
-  }
-
-  # Enable OS Zoom
-  class { 'osx::mouse::smart_zoom':
-    enabled => true
   }
 
   # Ensure bundler gem is installed for all ruby versions
