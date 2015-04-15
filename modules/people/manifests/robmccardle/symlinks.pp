f4efad5333a6448fbb9a83351e44e9f01d17c8ab
Rob McCardle <rob@enigma-marketing.co.uk>
2015-04-15 12:31:11 +0100

    Syntax

--- modules/people/manifests/robmccardle/symlinks.pp
+++ modules/people/manifests/robmccardle/symlinks.pp
@@ -5,9 +5,9 @@ class people::robmccardle::symlinks inherits people::robmccardle {
 
   	# Ensure a Symlink exists to the version controlled bash profile
 
-  	file { '$my_homedir/.bash_profile':
-  		ensure => 'link',
-  		target => '$my_homedir/.dotfiles/.bash_profile',
-	}
+  	#file { '$my_homedir/.bash_profile':
+  	#	ensure => 'link',
+  	#	target => '$my_homedir/.dotfiles/.bash_profile',
+	#}
 
 }
